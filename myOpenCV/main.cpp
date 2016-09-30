6ed008c40ffcdc222e83c85e1c2455920af30769
Katsuya <katsuya.f0515@gmail.com>
2016-09-30 18:39:24 +0900

    DOne

--- myOpenCV/main.cpp
+++ myOpenCV/main.cpp
@@ -12,34 +12,61 @@
 #include <iostream>
 #include <vector>
 
-
-int main () {
-    int corner_count = 150;
-    IplImage *dst_img, *src_img_gray, *eig_img, *temp_img;
-    CvPoint2D32f *corners;
-
-    dst_img = cvLoadImage("/Users/kty0515/Documents/Xcode/myOpenCV/myOpenCV/imgres.jpg", CV_LOAD_IMAGE_ANYCOLOR | CV_LOAD_IMAGE_ANYDEPTH);
-    src_img_gray = cvLoadImage("/Users/kty0515/Documents/Xcode/myOpenCV/myOpenCV/imgres.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-    eig_img = cvCreateImage(cvGetSize(src_img_gray), IPL_DEPTH_32F, 1);
-    temp_img = cvCreateImage(cvGetSize(src_img_gray), IPL_DEPTH_32F, 1);
-    corners = (CvPoint2D32f *) cvAlloc(corner_count * sizeof (CvPoint2D32f));
+int main(int argc, char** argv)
+{
+     CvPoint2D32f *corners;
     
-    // cvCornerMinEigenValを利用したコーナー検出
-    cvGoodFeaturesToTrack (src_img_gray, eig_img, temp_img, corners, &corner_count, 0.25, 15);
-    cvFindCornerSubPix (src_img_gray, corners, corner_count,
-                        cvSize (3, 3), cvSize (-1, -1), cvTermCriteria (CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 20, 0.03));
-    // 特徴点を描画
-    for (int i = 0; i < corner_count; i++){
-        printf("x=%4.0f y=%4.0f\n", corners[i].x, corners[i].y);
-        cvCircle(dst_img, cvPointFrom32f(corners[i]), 20, CV_RGB (0, 0, 255), 2);
+    cv::VideoCapture cap(0);
+    cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+    
+    const int cycle = 30;
+     int corner_count = 150;
+    cv::Mat prevFrame;
+    cv::Size frameSize = prevFrame.size();
+    cap >> prevFrame;
+    
+    cv::waitKey(cycle);
+    
+     IplImage dst_img, src_img_gray, *eig_img, *temp_img;
+    
+    while (1) {
+        cv::Mat frame;
+        cap >> frame;
+        
+        cv::Mat prevFrameGray;
+        cv::Mat currFrameGray;
+        
+        cv::cvtColor(prevFrame, prevFrameGray, CV_RGB2GRAY);
+        cv::cvtColor(frame, currFrameGray, CV_RGB2GRAY);
+        
+        // 特徴点抽出
+        std::vector<cv::Point2f> prevCorners;
+        std::vector<cv::Point2f> currCorners;
+        
+        dst_img = frame;
+        src_img_gray =currFrameGray;
+        eig_img = cvCreateImage(cvGetSize(&src_img_gray), IPL_DEPTH_32F, 1);
+        temp_img = cvCreateImage(cvGetSize(&src_img_gray), IPL_DEPTH_32F, 1);
+        
+        corners = (CvPoint2D32f *) cvAlloc(corner_count * sizeof (CvPoint2D32f));
+        
+        cvGoodFeaturesToTrack (&src_img_gray, eig_img, temp_img, corners, &corner_count, 0.05, 15);
+   
+    
+        cvFindCornerSubPix (&src_img_gray, corners, corner_count,
+                            cvSize (3, 3), cvSize (-1, -1), cvTermCriteria (CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 20, 0.03));
+        // 特徴点を描画
+        for (int i = 0; i < corner_count; i++){
+            printf("x=%4.0f y=%4.0f\n", corners[i].x, corners[i].y);
+            cvCircle(&dst_img, cvPointFrom32f(corners[i]), 2, CV_RGB (0, 0, 255), 2);
+        }
+
+        cvShowImage("image2", &dst_img);
+              prevFrame = frame;
+        if (cv::waitKey(cycle) == 27) { break; }
     }
-    
-    cvSaveImage("/Users/kty0515/Documents/Xcode/myOpenCV/o.png", dst_img);
-    
-    cvReleaseImage(&dst_img);
-    cvReleaseImage(&eig_img);
-    cvReleaseImage(&temp_img);
-    cvReleaseImage(&src_img_gray);
-    
     return 0;
 }
+
+
