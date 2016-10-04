4b7e003f156d4ffcfdc39abcc01aef63c5cae5c9
Katsuya <katsuya.f0515@gmail.com>
2016-10-05 01:18:11 +0900

    Implmetned

--- myOpenCV/main.cpp
+++ myOpenCV/main.cpp
@@ -7,53 +7,48 @@
 //
 
 #include "opencv2/opencv.hpp"
-#include "opencv2/highgui.hpp"
-
 #include <iostream>
 #include <vector>
+using namespace std;
 
 int main(int argc, char** argv)
 {
-     CvPoint2D32f *corners;
+    CvPoint2D32f *corners;
     
     cv::VideoCapture cap(0);
     cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
     cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
     
-    const int cycle = 30;
-     int corner_count = 150;
-    cv::Mat prevFrame;
-    cv::Size frameSize = prevFrame.size();
-    cap >> prevFrame;
-    
-    cv::waitKey(cycle);
-    
-     IplImage dst_img, src_img_gray, *eig_img, *temp_img;
+    int corner_count = 500;
+   
+
+    IplImage dst_img, src_img_gray, *eig_img, *temp_img;
     
     while (1) {
         cv::Mat frame;
         cap >> frame;
+        cv::Rect myROI(240, 80, 200, 200);
+        cv::Mat cut_img(frame,myROI);
         
-        cv::Mat prevFrameGray;
         cv::Mat currFrameGray;
+
         
-        cv::cvtColor(prevFrame, prevFrameGray, CV_RGB2GRAY);
-        cv::cvtColor(frame, currFrameGray, CV_RGB2GRAY);
+        cv::cvtColor(cut_img, currFrameGray, CV_RGB2GRAY);
         
         // 特徴点抽出
         std::vector<cv::Point2f> prevCorners;
         std::vector<cv::Point2f> currCorners;
         
-        dst_img = frame;
+        dst_img=cut_img;
         src_img_gray =currFrameGray;
         eig_img = cvCreateImage(cvGetSize(&src_img_gray), IPL_DEPTH_32F, 1);
         temp_img = cvCreateImage(cvGetSize(&src_img_gray), IPL_DEPTH_32F, 1);
         
         corners = (CvPoint2D32f *) cvAlloc(corner_count * sizeof (CvPoint2D32f));
         
-        cvGoodFeaturesToTrack (&src_img_gray, eig_img, temp_img, corners, &corner_count, 0.05, 15);
-   
-    
+        cvGoodFeaturesToTrack (&src_img_gray, eig_img, temp_img, corners, &corner_count, 0.15, 20);
+        
+        
         cvFindCornerSubPix (&src_img_gray, corners, corner_count,
                             cvSize (3, 3), cvSize (-1, -1), cvTermCriteria (CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 20, 0.03));
         // 特徴点を描画
@@ -61,11 +56,10 @@ int main(int argc, char** argv)
             printf("x=%4.0f y=%4.0f\n", corners[i].x, corners[i].y);
             cvCircle(&dst_img, cvPointFrom32f(corners[i]), 2, CV_RGB (0, 0, 255), 2);
         }
-
-        cvShowImage("image2", &dst_img);
-              prevFrame = frame;
-        if (cv::waitKey(cycle) == 27) { break; }
-    }
+        
+        cvShowImage("image", &dst_img);
+        cv::waitKey(30);
+           }
     return 0;
 }
 
